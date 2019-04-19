44cc46a0c99ab4d038ad1ed884cec8799c1da132
Julien Ruaux <jruaux@gmail.com>
2019-04-18 18:31:47 -0700

    initial commit

---
+++ search/src/main/java/com/redislabs/handsonlab/RediSearchApplication.java
@@ -0,0 +1,13 @@
+package com.redislabs.handsonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediSearchApplication.class, args);
+	}
+
+}
---
+++ streams/src/main/java/com/redislabs/handsonlab/RedisStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.redislabs.handsonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisStreamsApplication.class, args);
+	}
+
+}
