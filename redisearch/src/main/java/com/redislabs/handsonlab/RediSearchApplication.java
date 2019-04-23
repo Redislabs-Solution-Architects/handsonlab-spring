ea5d3fc01c60a51f47b3a14340e9c9b0b6535a5c
Julien Ruaux <jruaux@gmail.com>
2019-04-23 00:24:42 -0700

    added redisearch module

---
+++ redisearch/src/main/java/com/redislabs/handsonlab/RediSearchApplication.java
@@ -0,0 +1,36 @@
+package com.redislabs.handsonlab;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class RediSearchApplication implements ApplicationRunner {
+
+	@Autowired
+	StringRedisTemplate template;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediSearchApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Set the hello message in Redis
+
+		// TODO Retrieve the hello message from Redis
+		String message = "Not yet implemented";
+		log.info(message);
+
+		// TODO Increment a counter to keep track of the number of runs
+		long count = 0;
+		log.info("Hello Redis has been run {} times", count);
+	}
+
+}
